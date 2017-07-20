b35bdfacec211eed3211a319423a19c62e7a886d
SanjuBilly <sanjana22021998@gmail.com>
2017-07-20 23:28:25 +0530

    Add second approach to coin change problem

--- Algos/DP/coinChange.cpp
+++ Algos/DP/coinChange.cpp
@@ -9,21 +9,24 @@ int main() {
 	fill_n(minCoins,100, 100);
 	cin>>sum>>n;
 
-	// t = n;
-	for(i=0; i<n; i++){
+	for(i=0; i<n; i++)
 		cin>>coinVal[i];
-	}
 
 	minCoins[0] = 0;
-	for(i=1;i<=sum;i++) {
-		for(j=0; j<n; j++) {
-			if(coinVal[j] <= i && minCoins[i-coinVal[j]]+1 < minCoins[i]) {
+
+	// Approach 1
+
+	for(i=1;i<=sum;i++) 
+		for(j=0; j<n; j++) 
+			if(coinVal[j] <= i && minCoins[i-coinVal[j]]+1 < minCoins[i])
 				minCoins[i] = minCoins[i-coinVal[j]]+1;
-			}
-		}
-	}
+
+	// Approach 2
+
+	for(i=0; i<n; i++) 
+		for(j=coinVal[i] ; j<=sum; j++) 
+			minCoins[j] = minCoins[j-coinVal[i]] + 1;
 
 	cout<<minCoins[sum]<<"\n";
-
 	return 0;
 }
