e54266d1099a24f321a099957a90f6145d13a09a
sanjana098 <sanjana22021998@gmail.com>
2017-08-14 18:17:37 +0530

    Fix byteland goldcoins problem

--- SPOJ/coins.cpp
+++ SPOJ/coins.cpp
@@ -3,25 +3,30 @@ using namespace std;
 #ifndef lli
 #define lli long long int
 #endif
-vector<lli> v(1000000000, -1);
 
-lli best_exchange(lli n, vector<lli> v) {
-	if(n <= 4)
-		return n;
-	if(v[n] != -1)
+lli v[100001];
+
+lli best_exchange(lli n) {
+	if(n < 100001)
 		return v[n];
+
 	lli n2  = n/2, n3 = n/3, n4 = n/4;
-	return v[n] = max(n,(best_exchange(n2,v) + best_exchange(n3,v) + best_exchange(n4,v)));
+	return max(n,(best_exchange(n2) + best_exchange(n3) + best_exchange(n4)));
+	
 }
 
 int main() {
 
 	// http://www.spoj.com/problems/COINS/
-	lli n, best;
-	while(cin>>n) {
-		cout<<n<<"*\n";
+	lli n, best,i;
+	v[0] = 0;
+	for(i=1; i< 100000; i++) {
+		lli n2  = i/2, n3 = i/3, n4 = i/4;
+		v[i] = max(i, v[n2] + v[n3] + v[n4]);
+	}
 
-		best = best_exchange(n,v);
+	while(cin>>n) {
+		best = best_exchange(n);
 		cout<<best<<"\n";
 	}
 
